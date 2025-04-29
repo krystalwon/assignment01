38b6a97f3bc6fb1a4060135244acb868e92c48e1
Krystal Won <krystalw@upenn.edu>
2025-04-29 18:03:16 -0400

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -4,5 +4,10 @@
     Your query should have a single record with a single attribute, the number
     of stations (num_stations).
 */
-
--- Enter your SQL query here
+SELECT 
+    COUNT(*) AS stations_within_1km
+FROM 
+    indego.station_statuses
+WHERE
+    geog IS NOT NULL
+    AND ST_Distance(geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)::geography) <= 1000;
