789fafc48cef377afbfc85d7d5fc7eae180b1b87
Krystal Won <krystalw@upenn.edu>
2025-04-29 18:04:17 -0400

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -6,4 +6,14 @@
     Meyerson Hall, rounded to the nearest 50 meters.
 */
 
--- Enter your SQL query here
+SELECT 
+    id,
+    name,
+    ST_Distance(geog, ST_SetSRID(ST_MakePoint(-75.192584, 39.952415), 4326)::geography) AS distance_meters
+FROM 
+    indego.station_statuses
+WHERE
+    geog IS NOT NULL
+ORDER BY 
+    distance_meters
+LIMIT 1;
