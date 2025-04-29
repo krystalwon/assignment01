3a9860a40df60664230582d89f392d7108f43b0d
Krystal Won <krystalw@upenn.edu>
2025-04-29 17:30:32 -0400

    Update query07.sql

--- query07.sql
+++ query07.sql
@@ -5,8 +5,23 @@
     trip_quarter, and one column named num_trips.
 */
 
--- Enter your SQL query here
-
+SELECT 
+    EXTRACT(YEAR FROM start_time) AS trip_year,
+    EXTRACT(QUARTER FROM start_time) AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM (
+    SELECT start_time, end_time
+    FROM indego.trips_2021_q3
+    WHERE DATE(start_time) <> DATE(end_time)
+    
+    UNION ALL
+    
+    SELECT start_time, end_time
+    FROM indego.trips_2022_q3
+    WHERE DATE(start_time) <> DATE(end_time)
+) AS multi_day_trips
+GROUP BY trip_year, trip_quarter
+ORDER BY trip_year, trip_quarter;
 
 
 /*
