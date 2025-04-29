cc9c0eff4420123eba0fc9d213a637f4428c2745
Krystal Won <krystalw@upenn.edu>
2025-04-29 17:29:56 -0400

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -5,5 +5,20 @@
     (named `trip_year`), one for the quarter (named `trip_quarter`), and one for
     the number of trips (named `num_trips`).
 */
-
--- Enter your SQL query here
+SELECT 
+    2021 AS trip_year,
+    3 AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM 
+    indego.trips_2021_q3
+WHERE 
+    duration < 600
+UNION ALL
+SELECT 
+    2022 AS trip_year,
+    3 AS trip_quarter,
+    COUNT(*) AS num_trips
+FROM 
+    indego.trips_2022_q3
+WHERE 
+    duration < 600;
