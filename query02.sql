efe417823a1c26bbdec45c1831849ad6c0ab392a
Krystal Won <krystalw@upenn.edu>
2025-04-29 20:37:26 -0400

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -8,16 +8,23 @@
 
     Remember you can do calculations in the select clause.
 */
-
 WITH 
-    trips_2021 AS (SELECT COUNT(*) AS count_2021 FROM indego.trips_2021_q3),
-    trips_2022 AS (SELECT COUNT(*) AS count_2022 FROM indego.trips_2022_q3)
-SELECT 
-    ROUND(((trips_2022.count_2022 - trips_2021.count_2021) * 100.0 / trips_2021.count_2021)::numeric, 2) AS perc_change
+    trips_2021 AS (
+        SELECT COUNT(*) AS count_2021 
+        FROM indego.trips_2021_q3
+    ),
+    trips_2022 AS (
+        SELECT COUNT(*) AS count_2022 
+        FROM indego.trips_2022_q3
+    )
+SELECT
+    ROUND(
+        ((trips_2022.count_2022 - trips_2021.count_2021) * 100.0 / trips_2021.count_2021)::numeric, 
+        2
+    ) AS perc_change
 FROM 
-    trips_2021, trips_2022;
-
-
+    trips_2021, 
+    trips_2022;
 /*
     If you want to get fancier here, you can cast the result to a string and
     concatenate a '%' to the end. For example:
